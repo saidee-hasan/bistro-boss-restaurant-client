32c2d1be98a85958ef27f9405db55ca171ad6419
saideeWD <mdsaideehasan@gmail.com>
2025-01-05 20:44:45 +0600

    costome hooks dpne

--- src/components/PopularMenu.jsx
+++ src/components/PopularMenu.jsx
@@ -1,24 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import SectionTitle from './SectionTitle'
 import MenuItems from './MenuItems'
+import useMenu from '../hooks/useMenu'
 
 export default function PopularMenu() {
-const [menu,setMenu]=useState([])
-  useEffect(()=>{
-    fetch('../menu.json')
-    .then(res=>res.json())
-    .then(data=>{
-      const popularItems = data.filter(item=>item.category === "popular")
- setMenu(popularItems)
-    })
+const [menu,loading]=useMenu()
+const popularItems = menu.filter(item=>item.category === "popular")
 
-  },[])
   return (
     <div>
       <SectionTitle  subHeading={"---Check it out---"} heading={"FROM OUR MENU"} />
 <div className="grid md:grid-cols-2 gap-10 mt-5  p-2">
   {
-    menu.map((item,idx)=> (<MenuItems item={item} key={idx}/>))
+    popularItems.map((item,idx)=> (<MenuItems item={item} key={idx}/>))
   }
 
 </div>
